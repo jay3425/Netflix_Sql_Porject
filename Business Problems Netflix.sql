2cb6c0056bd4717825f45f94bf8d61b9c58e74b8
Jay_Dengle <jaydengle2005@gmail.com>
2025-06-02 13:44:00 +0530

    Update Business Problems Netflix.sql

--- Business Problems Netflix.sql
+++ Business Problems Netflix.sql
@@ -1,21 +1,25 @@
--- 15 Business Problems & Solutions
-
-1. Count the number of Movies vs TV Shows
-2. Find the most common rating for movies and TV shows
-3. List all movies released in a specific year (e.g., 2020)
-4. Find the top 5 countries with the most content on Netflix
-5. Identify the longest movie
-6. Find content added in the last 5 years
-7. Find all the movies/TV shows by director 'Rajiv Chilaka'!
-8. List all TV shows with more than 5 seasons
-9. Count the number of content items in each genre
-10.Find each year and the average numbers of content release in India on netflix. 
+-- 20 Business Problems & Solutions
+1. What is the average duration of movies by rating?
+2. Which countries produce the most TV Shows vs Movies?
+3. Find directors with the most Netflix content
+4. Track Netflix content growth over time
+5. What are the top 10 most common genres?
+6. Count the number of Movies vs TV Shows
+7. Find the most common rating for movies and TV shows
+8. List all movies released in a specific year (e.g., 2020)
+9. Find the top 5 countries with the most content on Netflix
+10. Identify the longest movie
+11. Find content added in the last 5 years
+12. Find all the movies/TV shows by director 'Rajiv Chilaka'!
+13. List all TV shows with more than 5 seasons
+14. Count the number of content items in each genre
+15.Find each year and the average numbers of content release in India on netflix. 
 return top 5 year with highest avg content release!
-11. List all movies that are documentaries
-12. Find all content without a director
-13. Find how many movies actor 'Salman Khan' appeared in last 10 years!
-14. Find the top 10 actors who have appeared in the highest number of movies produced in India.
-15.
+16. List all movies that are documentaries
+17. Find all content without a director
+18. Find how many movies actor 'Salman Khan' appeared in last 10 years!
+19. Find the top 10 actors who have appeared in the highest number of movies produced in India.
+20.
 Categorize the content based on the presence of the keywords 'kill' and 'violence' in 
 the description field. Label content containing these keywords as 'Bad' and all other 
 content as 'Good'. Count how many items fall into each category.
